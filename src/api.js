6a5fca7bf60dc93bb74f87acb3d49b259d81def6
GMoreirasks <moreirasks@gmail.com>
2024-12-04 15:27:14 -0300

    alt

--- src/api.js
+++ src/api.js
@@ -1,8 +1,10 @@
-
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backsistemareserva.onrender.com', // URL do seu backend hospedado no Render
+  baseURL: 'https://backsistemareserva.onrender.com', // URL base da sua API
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
